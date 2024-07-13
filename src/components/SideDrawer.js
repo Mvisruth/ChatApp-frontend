b8fb2cf3f36e2a0ef5109118b68e285c5cd654e5
Mvisruth <visruthm123@gmail.com>
2024-07-13 23:31:13 +0530

    update ChatPage

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -2,18 +2,37 @@ import { Box, Tooltip,Typography } from '@mui/material'
 import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 
 
 function SideDrawer() {
   const [search,setSearch]=useState("")
   const [searchResult,serSearchResult]=useState([])
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div>
-      <Box>
+      <Box
+      display={"flex"}
+      justifyContent={'space-between'}
+      alignItems={"center"}
+      bgcolor={"white"}
+      width={"100%"}
+      p={"5px 10px 5px 10px"}  
+      border={"5px"}    
+      >
         <Tooltip title="Search Users to Chat" placement="top">
           <ButtonGroup variant="text" aria-label="Basic button group">
-            <Button>
+            <Button className='text-dark'>
               <i class="fa-solid fa-magnifying-glass"></i>
               <Typography
               sx={{
@@ -21,13 +40,43 @@ function SideDrawer() {
                 textTransform: 'lowercase', // Transform text to lowercase
                 marginLeft: 1 // Add some margin to separate text from icon
               }}
-             
-            >    
+             >    
              search user
              </Typography>       
              </Button>
           </ButtonGroup>
        </Tooltip>
+       <text className='fs-2 ' fontFamily='Work-sans'>ChatMe</text>
+       <div>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <CircleNotificationsIcon className='fs-2'/>
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
       </Box>
     </div>
     
