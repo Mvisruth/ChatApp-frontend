c5d7c1a32a6938f33348de4702e5291ba96650ce
Mvisruth <visruthm123@gmail.com>
2024-07-13 22:48:39 +0530

    create SiderDraw

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,37 +1,37 @@
-// import { Box, Tooltip,Typography } from '@mui/material'
-// import React, { useState } from 'react'
-// import Button from '@mui/material/Button';
-// import ButtonGroup from '@mui/material/ButtonGroup';
+import { Box, Tooltip,Typography } from '@mui/material'
+import React, { useState } from 'react'
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 
-// function SideDrawer() {
-//   const [search,setSearch]=useState("")
-//   const [searchResult,serSearchResult]=useState([])
+function SideDrawer() {
+  const [search,setSearch]=useState("")
+  const [searchResult,serSearchResult]=useState([])
 
-//   return (
-//     <div>
-//       <Box>
-//         <Tooltip title="Search Users to Chat" placement="top">
-//           <ButtonGroup variant="text" aria-label="Basic button group">
-//             <Button>
-//               <i class="fa-solid fa-magnifying-glass"></i>
-//               <Typography
-//               sx={{
-//                 display: { base: 'none', md: 'flex' },
-//                 textTransform: 'lowercase', // Transform text to lowercase
-//                 marginLeft: 1 // Add some margin to separate text from icon
-//               }}
+  return (
+    <div>
+      <Box>
+        <Tooltip title="Search Users to Chat" placement="top">
+          <ButtonGroup variant="text" aria-label="Basic button group">
+            <Button>
+              <i class="fa-solid fa-magnifying-glass"></i>
+              <Typography
+              sx={{
+                display: { base: 'none', md: 'flex' },
+                textTransform: 'lowercase', // Transform text to lowercase
+                marginLeft: 1 // Add some margin to separate text from icon
+              }}
              
-//             >    
-//              search user
-//              </Typography>       
-//              </Button>
-//           </ButtonGroup>
-//        </Tooltip>
-//       </Box>
-//     </div>
+            >    
+             search user
+             </Typography>       
+             </Button>
+          </ButtonGroup>
+       </Tooltip>
+      </Box>
+    </div>
     
-//   )
-// }
+  )
+}
 
-// export default SideDrawer
+export default SideDrawer
